f7bfefb722fe13db630c1c1bc741200000551879
pijenn <rafiezein2110@gmail.com>
2023-11-18 14:14:32 +0700

    perbaiki landing

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
             <div
-                className="h-screen bg-cover flex items-center"
+                className="h-screen bg-cover flex items-center font-poppins"
                 style={{ backgroundImage: `url(${backgroundStyle})` }}
             >
                 <div className="w-full">
@@ -15,14 +15,16 @@ const Home = () => {
                     <div className="w-fit mx-auto text-red-600 text-[100px] font-bold mt-1 shadow-lg">
                         PRESENSI FOR PIT
                     </div>
-                    <div className="w-fit mx-auto text-white mt-4 text-[28px]" >
+                    <div className="w-6/12 mx-auto text-white mt-4 text-[28px] text-center ">
                         Website absensi yang dikhususkan untuk Kebiroan
                         Pengembangan Informasi dan Teknologi Badan Eksekutif
                         Mahasiswa Fakultas Ilmu Komputer Universitas Brawijaya.
                         Jangan lupa absen!!
                     </div>
-                    <div className="w-fit mx-auto">
-                        <button className="bg-red-500 hover:bg-red-400 text-white px-20 py-2 rounded-full active:bg-red-700 text-[24px]">Mulai</button>
+                    <div className="w-fit mx-auto mt-8">
+                        <button className="bg-red-500 hover:bg-red-400 text-white px-20 py-2 rounded-full active:bg-red-700 text-[24px]">
+                            Mulai
+                        </button>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        extend: {},
+    theme: { 
+        extend: {
+            colors:{
+                "primary":"ee333d",
+            },
+            fontFamily: {
+                poppins: ["Poppins", "sans-serif"],
+            },
+
+        },
     },
     plugins: [],
 };
